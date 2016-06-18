2bd0e827595939205872dbadb850773b18e4aa8a
Exitare <exi@pihaar.de>
2016-06-18 10:23:27 +0200

    [Custom] Modify

--- src/server/scripts/Custom/ex_commands.cpp
+++ src/server/scripts/Custom/ex_commands.cpp
@@ -44,33 +44,131 @@ public:
 	static bool HandleBotAddCommand(ChatHandler* handler, const char* args)
 	{
 
-		char* px = strtok((char*)args, " ");
-		uint32 npcid = (uint32)atoi(px);
+		if (!*args)
+			return false;
 
-		if (npcid != 800059){
+		char* charID = handler->extractKeyFromLink((char*)args, "Hcreature_entry");
+		if (!charID)
+			return false;
+
+		uint32 id = atoi(charID);
+
+		if (id != 800058){
 			return false;
 		}
 
-		if (!sObjectMgr->GetCreatureTemplate(npcid)){
+		if (!sObjectMgr->GetCreatureTemplate(id))
+			return false;
+
+		Player* chr = handler->GetSession()->GetPlayer();
+		float x = chr->GetPositionX();
+		float y = chr->GetPositionY();
+		float z = chr->GetPositionZ();
+		float o = chr->GetOrientation();
+		Map* map = chr->GetMap();
+
+		if (Transport* trans = chr->GetTransport())
+		{
+			ObjectGuid::LowType guid = map->GenerateLowGuid<HighGuid::Unit>();
+			CreatureData& data = sObjectMgr->NewOrExistCreatureData(guid);
+			data.id = id;
+			data.phaseMask = chr->GetPhaseMaskForSpawn();
+			data.posX = chr->GetTransOffsetX();
+			data.posY = chr->GetTransOffsetY();
+			data.posZ = chr->GetTransOffsetZ();
+			data.orientation = chr->GetTransOffsetO();
+
+			Creature* creature = trans->CreateNPCPassenger(guid, &data);
+
+			creature->SaveToDB(trans->GetGOInfo()->moTransport.mapID, 1 << map->GetSpawnMode(), chr->GetPhaseMaskForSpawn());
+
+			sObjectMgr->AddCreatureToGrid(guid, &data);
+			return true;
+		}
+
+		Creature* creature = new Creature();
+		if (!creature->Create(map->GenerateLowGuid<HighGuid::Unit>(), map, chr->GetPhaseMaskForSpawn(), id, x, y, z, o))
+		{
+			delete creature;
 			return false;
 		}
 
+		creature->SaveToDB(map->GetId(), (1 << map->GetSpawnMode()), chr->GetPhaseMaskForSpawn());
 
-		Player* player;
-		player->SummonCreature(npcid, player->GetPositionX() + 5, player->GetPositionY(), player->GetPositionZ() + 5, 0, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN,120000);
+		ObjectGuid::LowType db_guid = creature->GetSpawnId();
 
+		// To call _LoadGoods(); _LoadQuests(); CreateTrainerSpells()
+		// current "creature" variable is deleted and created fresh new, otherwise old values might trigger asserts or cause undefined behavior
+		creature->CleanupsBeforeDelete();
+		delete creature;
+		creature = new Creature();
+		if (!creature->LoadCreatureFromDB(db_guid, map))
+		{
+			delete creature;
+			return false;
+		}
+
+		sObjectMgr->AddCreatureToGrid(db_guid, sObjectMgr->GetCreatureData(db_guid));
+		return true;
 	};
 
 
-	static bool HandleBotDelCommand(ChatHandler* handler, const char* /*args*/)
+	static bool HandleBotDelCommand(ChatHandler* handler, const char* args)
 	{
+		Creature* unit = nullptr;
+		Creature* creature = handler->getSelectedCreature();
 
+		if (creature->GetCreatureTemplate()->Entry != 800058){
+			return false;
+		}
+
+		if (*args)
+		{
+			// number or [name] Shift-click form |color|Hcreature:creature_guid|h[name]|h|r
+			char* cId = handler->extractKeyFromLink((char*)args, "Hcreature");
+			if (!cId)
+				return false;
+
+			ObjectGuid::LowType lowguid = atoi(cId);
+			if (!lowguid)
+				return false;
+
+			if (CreatureData const* cr_data = sObjectMgr->GetCreatureData(lowguid))
+				unit = handler->GetSession()->GetPlayer()->GetMap()->GetCreature(ObjectGuid(HighGuid::Unit, cr_data->id, lowguid));
+		}
+		else
+			unit = handler->getSelectedCreature();
+
+		if (!unit || unit->IsPet() || unit->IsTotem())
+		{
+			handler->SendSysMessage(LANG_SELECT_CREATURE);
+			handler->SetSentErrorMessage(true);
+			return false;
+		}
+
+		// Delete the creature
+		unit->CombatStop();
+		unit->DeleteFromDB();
+		unit->AddObjectToRemoveList();
+
+		handler->SendSysMessage(LANG_COMMAND_DELCREATMESSAGE);
+
+		return true;
 	};
 
 
 	static bool HandleBotAttackCommand(ChatHandler* handler, const char* /*args*/)
 	{
 		
+		Unit* victim;
+		victim->SelectNearbyTarget();
+		Creature* target = handler->getSelectedCreature();
+		if (!target){
+			return false;
+		}
+
+		target->setFaction(1);
+		target->Attack(victim,true);
 	};
 
 
