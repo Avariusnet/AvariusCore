6d0f7c959e3df141522e272efbefc9483c208c55
Exitare <exi@pihaar.de>
2016-06-18 10:30:13 +0200

    [Custom] add missing includes

--- src/server/scripts/Custom/ex_commands.cpp
+++ src/server/scripts/Custom/ex_commands.cpp
@@ -8,11 +8,17 @@
 #include "Log.h"
 #include "SpellAuras.h"
 #include "World.h"
-
+#include "Transport.h"
 #include "ScriptMgr.h"
 #include "ObjectMgr.h"
 #include "Chat.h"
 #include "SocialMgr.h"
+#include "CreatureGroups.h"
+#include "Language.h"
+#include "TargetedMovementGenerator.h"                      
+#include "CreatureAI.h"
+#include "Player.h"
+#include "Pet.h"
 
 class ex_commands : public CommandScript
 {
