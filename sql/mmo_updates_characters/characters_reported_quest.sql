52d390370bc24cc8d2006d76d1b2007673205f7d
Exitare <exi@pihaar.de>
2016-07-10 21:13:06 +0200

    [SQL] Add new Table

---
+++ sql/mmo_updates_characters/characters_reported_quest.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `reported_quest` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `questid` INT NULL COMMENT '',
+  `anzahl` INT NULL COMMENT '',
+  `aktiv` INT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '');
