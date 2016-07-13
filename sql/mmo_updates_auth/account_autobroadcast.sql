49f555c2ba96be7aaf11ca407a019374641a17e7
Exitare <exi@pihaar.de>
2016-07-13 13:58:45 +0200

    [Custom] Insert player_reported_quest

--- sql/mmo_updates_auth/account_autobroadcast.sql
+++ sql/mmo_updates_auth/account_autobroadcast.sql
@@ -1,3 +1,4 @@
-Delete from `broadcast` where id between 1 and x;
+Delete from `autobroadcast` where id between 1 and 2;
 
-INSERT INTO `broadcast` Values (1,1,1,"Report buggy Quests with .bug report [shift+left click on quest]. Then press Return");
+INSERT INTO `autobroadcast` Values (1,1,3,"Report buggy Quests with .bug report [shift+left click on quest]. Then press Return");
+INSERT INTO `autobroadcast` Values (1,2,2,"Get your free Firstcharacter now! Click on Exaltor and get your Upgrade!");
--- src/server/scripts/Custom/ex_commands.cpp
+++ src/server/scripts/Custom/ex_commands.cpp
@@ -80,102 +80,120 @@ public:
         
         uint32 questId = 0;
         
-        if (args[0] == '[')                                        // [name] manual form
-        {
-            char const* questNameStr = strtok((char*)args, "]");
-            
-            if (questNameStr && questNameStr[0])
-            {
-                std::string questName = questNameStr+1;
-                WorldDatabase.EscapeString(questName);
-                
-                PreparedStatement* stmt = WorldDatabase.GetPreparedStatement(WORLD_SEL_QUESTID_BY_NAME);
-                stmt->setString(0, questName);
-                PreparedQueryResult result = WorldDatabase.Query(stmt);
+		if (args[0] == '[')                                        // [name] manual form
+		{
+			char const* questNameStr = strtok((char*)args, "]");
 
-                if(!result){
-                    player->GetSession()->SendNotification("Error beim Abfragen der Quest");
-                    return true;
-                }
-                
-				
+			if (questNameStr && questNameStr[0])
+			{
+				std::string questName = questNameStr + 1;
+				WorldDatabase.EscapeString(questName);
 
-						
-                Field* questnr = result->Fetch();
-                uint32 questid = questnr[0].GetInt32();
-                
-		//check if player reported quest already. if true -> return false. if false -> insert in db and report quest.	
-								
+				PreparedStatement* stmt = WorldDatabase.GetPreparedStatement(WORLD_SEL_QUESTID_BY_NAME);
+				stmt->setString(0, questName);
+				PreparedQueryResult result = WorldDatabase.Query(stmt);
 
-                //CHECK IF QUEST WITH ID IS IN DB
-                PreparedStatement * selreportquest = CharacterDatabase.GetPreparedStatement(CHAR_SEL_REPORT_QUEST);
-                selreportquest->setInt32(0,questid);
-                PreparedQueryResult ergebnis = CharacterDatabase.Query(selreportquest);
-                
-                //NO Quest with Id in DB
-                if(!ergebnis){
-					
-                    PreparedStatement* insertnewquest = CharacterDatabase.GetPreparedStatement(CHAR_INS_REPORT_QUEST);
+				if (!result){
+					player->GetSession()->SendNotification("Error beim Abfragen der Quest");
+					return true;
+				}
+
+
+
+
+				Field* questnr = result->Fetch();
+				uint32 questid = questnr[0].GetInt32();
+
+				//check if player reported quest already. if true -> return false. if false -> insert in db and report quest.	
+				PreparedStatement * selreportquestplayer = CharacterDatabase.GetPreparedStatement(CHAR_SEL_PLAYER_REPORT_QUEST);
+				selreportquestplayer->setInt32(0, questid);
+				PreparedQueryResult existplayer = CharacterDatabase.Query(selreportquestplayer);
+
+				if (!existplayer){
+
+
+					//Insert player in reported_quest_player db
+
+					PreparedStatement* insertnewplayer = CharacterDatabase.GetPreparedStatement(CHAR_INS_PLAYER_REPORT_QUEST);
+					insertnewplayer->setString(0, player->GetSession()->GetPlayerName());
+					insertnewplayer->setInt32(1, player->GetGUID());
+					insertnewplayer->setInt32(2, questid);
+					CharacterDatabase.Execute(insertnewplayer);
+					player->GetSession()->SendNotification("Spieler eingetragen");
+
+					//CHECK IF QUEST WITH ID IS IN DB
+					PreparedStatement * selreportquest = CharacterDatabase.GetPreparedStatement(CHAR_SEL_REPORT_QUEST);
+					selreportquest->setInt32(0, questid);
+					PreparedQueryResult ergebnis = CharacterDatabase.Query(selreportquest);
+
+				//NO Quest with Id in DB
+				if (!ergebnis){
+
+					PreparedStatement* insertnewquest = CharacterDatabase.GetPreparedStatement(CHAR_INS_REPORT_QUEST);
 					insertnewquest->setString(0, questName);
-					insertnewquest->setInt32(1,questid);
-                    insertnewquest->setInt32(2,1);
-                    insertnewquest->setInt32(3, 0);
-                    CharacterDatabase.Execute(insertnewquest);
-                    player->GetSession()->SendNotification("Die Quest wurde erfolgreich eingetragen");
-                    return true;
-                }
-                
-                //FETCH DB DATA
-				
+					insertnewquest->setInt32(1, questid);
+					insertnewquest->setInt32(2, 1);
+					insertnewquest->setInt32(3, 0);
+					CharacterDatabase.Execute(insertnewquest);
+					player->GetSession()->SendNotification("Die Quest wurde erfolgreich eingetragen");
+					return true;
+				}
 
-				
-                Field* report_quest = ergebnis->Fetch();
-                uint32 questreportid = report_quest[0].GetInt32();
-                uint32 anzahl = report_quest[1].GetInt32();
-                uint32 aktiv = report_quest[2].GetInt32();
-                
-                
-                //Update anzahl, und aktiv auf 1 setzen.
-                if(anzahl == 5){
-                    PreparedStatement * updatequestaktiv = CharacterDatabase.GetPreparedStatement(CHAR_UPD_REPORT_QUEST_ACTIVE);
-                    updatequestaktiv->setInt32(0,anzahl+1);
-                    updatequestaktiv->setInt32(1,1);
-                    updatequestaktiv->setInt32(2,questreportid);
-                    CharacterDatabase.Execute(updatequestaktiv);
-                    
-                    
-                    const Quest* quest = sObjectMgr->GetQuestTemplate(questreportid);
-                    //TODO PLayer can complete and reward quest
-                    player->CanCompleteQuest(questreportid);
-                    player->CanRewardQuest(quest, false);
-                    player->GetSession()->SendNotification("Die Quest wurde erfolgreich reported und die Quest abgeschlossen.");
-                    return true;
-                }
-                
-                //aktiv == 1, quest abschließen und counter um 1 erhöhen.
-                if(aktiv == 1){
-                    PreparedStatement * updatequest = CharacterDatabase.GetPreparedStatement(CHAR_UPD_REPORT_QUEST);
-                    updatequest->setInt32(0,anzahl+1);
-                    updatequest->setInt32(1,questreportid);
-                    CharacterDatabase.Execute(updatequest);
-                    player->GetSession()->SendNotification("Die Quest wurde erfolgreich reportet und abgeschlossen.");
-                    return true;
-                }
-                
-                
-                //weder counter == 5 noch aktiv == 1
-                PreparedStatement * updatequest = CharacterDatabase.GetPreparedStatement(CHAR_UPD_REPORT_QUEST);
-                updatequest->setInt32(0,anzahl+1);
-                updatequest->setInt32(1,questreportid);
-                CharacterDatabase.Execute(updatequest);
-                player->GetSession()->SendNotification("Quest erfolgreich reportet.");
-                
-                
-                return true;
-                
-            }
+				//FETCH DB DATA
 
-            return true;
+
+
+				Field* report_quest = ergebnis->Fetch();
+				uint32 questreportid = report_quest[0].GetInt32();
+				uint32 anzahl = report_quest[1].GetInt32();
+				uint32 aktiv = report_quest[2].GetInt32();
+
+
+				//Update anzahl, und aktiv auf 1 setzen.
+				if (anzahl == 5){
+					PreparedStatement * updatequestaktiv = CharacterDatabase.GetPreparedStatement(CHAR_UPD_REPORT_QUEST_ACTIVE);
+					updatequestaktiv->setInt32(0, anzahl + 1);
+					updatequestaktiv->setInt32(1, 1);
+					updatequestaktiv->setInt32(2, questreportid);
+					CharacterDatabase.Execute(updatequestaktiv);
+
+
+					const Quest* quest = sObjectMgr->GetQuestTemplate(questreportid);
+					//TODO PLayer can complete and reward quest
+					player->CanCompleteQuest(questreportid);
+					player->CanRewardQuest(quest, false);
+					player->GetSession()->SendNotification("Die Quest wurde erfolgreich reported und die Quest abgeschlossen.");
+					return true;
+				}
+
+				//aktiv == 1, quest abschließen und counter um 1 erhöhen.
+				if (aktiv == 1){
+					PreparedStatement * updatequest = CharacterDatabase.GetPreparedStatement(CHAR_UPD_REPORT_QUEST);
+					updatequest->setInt32(0, anzahl + 1);
+					updatequest->setInt32(1, questreportid);
+					CharacterDatabase.Execute(updatequest);
+					player->GetSession()->SendNotification("Die Quest wurde erfolgreich reportet und abgeschlossen.");
+					return true;
+				}
+
+
+				//weder counter == 5 noch aktiv == 1
+				PreparedStatement * updatequest = CharacterDatabase.GetPreparedStatement(CHAR_UPD_REPORT_QUEST);
+				updatequest->setInt32(0, anzahl + 1);
+				updatequest->setInt32(1, questreportid);
+				CharacterDatabase.Execute(updatequest);
+				player->GetSession()->SendNotification("Quest erfolgreich reportet.");
+
+
+				return true;
+
+			}
+
+			return true;
+			}
+
+			//Player already report quest
+			player->GetSession()->SendNotification("Quest reported");
         }
         
         
@@ -203,20 +221,36 @@ public:
 			selreportquest->setInt32(0, questid);
 			PreparedQueryResult ergebnis = CharacterDatabase.Query(selreportquest);
 
-		
 
-			//NO Quest with Id in DB
-			if (!ergebnis){
-				
-				PreparedStatement* insertnewquest = CharacterDatabase.GetPreparedStatement(CHAR_INS_REPORT_QUEST);
-				insertnewquest->setString(0, questname);
-				insertnewquest->setInt32(1, questid);
-				insertnewquest->setInt32(2, 1);
-				insertnewquest->setInt32(3, 0);
-				CharacterDatabase.Execute(insertnewquest);
-				player->GetSession()->SendNotification("Die Quest wurde erfolgreich eingetragen");
-				return true;
-			}
+			//check if player reported quest already. if true -> return false. if false -> insert in db and report quest.	
+			PreparedStatement * selreportquestplayer = CharacterDatabase.GetPreparedStatement(CHAR_SEL_PLAYER_REPORT_QUEST);
+			selreportquestplayer->setInt32(0, questid);
+			PreparedQueryResult existplayer = CharacterDatabase.Query(selreportquestplayer);
+
+			if (!existplayer){
+
+				//Insert player in reported_quest_player db
+
+				PreparedStatement* insertnewplayer = CharacterDatabase.GetPreparedStatement(CHAR_INS_PLAYER_REPORT_QUEST);
+				insertnewplayer->setString(0, player->GetSession()->GetPlayerName());
+				insertnewplayer->setInt32(1, player->GetGUID());
+				insertnewplayer->setInt32(2, questid);
+				CharacterDatabase.Execute(insertnewplayer);
+				player->GetSession()->SendNotification("Spieler eingetragen");
+
+
+				//NO Quest with Id in DB
+				if (!ergebnis){
+
+					PreparedStatement* insertnewquest = CharacterDatabase.GetPreparedStatement(CHAR_INS_REPORT_QUEST);
+					insertnewquest->setString(0, questname);
+					insertnewquest->setInt32(1, questid);
+					insertnewquest->setInt32(2, 1);
+					insertnewquest->setInt32(3, 0);
+					CharacterDatabase.Execute(insertnewquest);
+					player->GetSession()->SendNotification("Die Quest wurde erfolgreich eingetragen");
+					return true;
+				}
 
 			//FETCH DB DATA
 			Field* report_quest = ergebnis->Fetch();
@@ -262,7 +296,11 @@ public:
 
 
 			return true;
+			}
 
+			else{
+				player->GetSession()->SendNotification("Quest schon reported");
+			}
 		}
                 
         return true;
