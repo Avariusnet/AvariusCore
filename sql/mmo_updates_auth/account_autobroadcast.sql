19b1cc3df8cd4f1820dc4e4c98efe271f89c35c7
Exitare <business.exitare@gmail.com>
2016-07-17 23:11:42 +0200

    [SQL] Add new Broadcast

--- sql/mmo_updates_auth/account_autobroadcast.sql
+++ sql/mmo_updates_auth/account_autobroadcast.sql
@@ -1,8 +1,8 @@
-Delete from `autobroadcast` where id between 1 and 5;
+Delete from `autobroadcast` where id between 1 and 6;
 
 INSERT INTO `autobroadcast` Values (1,1,2,"Report buggy Quests with .report quest [shift+left click on quest]. Then press Return");
 INSERT INTO `autobroadcast` Values (1,2,2,"Get your free Firstcharacter now! Click on Exaltor and get your Upgrade!");
 INSERT INTO `autobroadcast` Values (1,3,2,"Take a look on our new Race. Wandervolk. Now available. Type ".elite insel" to get the start quest.");
 INSERT INTO `autobroadcast` Values (1,4,2,"Do you need Support? In Orgrimmar and Stormwind is our Support-Npc. Talk to her and you will get help.");
 INSERT INTO `autobroadcast` Values (1,5,2,"To start the extra Content, just kill the endboss of one northend dungeon. Except of Utgard Dungeons. Endbosses will drop the questitem.");
-
+INSERT INTO `autobroadcast` Values (1,6,2,"Portet Euch mit .ma oder .da nach Orgrimmar/Sturmwind oder Dalaran.");
