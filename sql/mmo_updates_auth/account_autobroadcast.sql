0f10fac7d822d02800734185b152ca5c8673ce2d
Exitare <business.exitare@icloud.com>
2016-07-19 22:16:36 +0200

    [SQL] Edit Autobroadcast

--- sql/mmo_updates_auth/account_autobroadcast.sql
+++ sql/mmo_updates_auth/account_autobroadcast.sql
@@ -4,5 +4,5 @@ INSERT INTO `autobroadcast` Values (1,1,2,"Report buggy Quests with .report ques
 INSERT INTO `autobroadcast` Values (1,2,2,"Get your free Firstcharacter now! Click on Exaltor and get your Upgrade!");
 INSERT INTO `autobroadcast` Values (1,3,2,"Take a look on our new Race. Wandervolk. Now available. Type ".elite insel" to get the start quest.");
 INSERT INTO `autobroadcast` Values (1,4,2,"Do you need Support? In Orgrimmar and Stormwind is our Support-Npc. Talk to her and you will get help.");
-INSERT INTO `autobroadcast` Values (1,5,2,"To start the extra Content, just kill the endboss of one northend dungeon. Except of Utgard Dungeons. Endbosses will drop the questitem.");
+INSERT INTO `autobroadcast` Values (1,5,2,"Um den Extracontent zu starten, besiegt die Endbosse jedes Nordenddungeons (außer Turm und Burg Utgarde) um das Startitem zu bekommen.");
 INSERT INTO `autobroadcast` Values (1,6,2,"Portet Euch mit .ma oder .da nach Orgrimmar/Sturmwind oder Dalaran.");
