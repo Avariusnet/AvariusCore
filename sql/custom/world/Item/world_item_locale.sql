ec2abdd77c9b08fb6b8ed2d401fb26199673606e
Exitare <business.exitare@icloud.com>
2017-03-22 14:48:09 +0100

    Fix sql

--- sql/custom/world/Item/world_item_locale.sql
+++ sql/custom/world/Item/world_item_locale.sql
@@ -3,4 +3,4 @@ INSERT INTO `locales_item` (`entry`, `name_loc3`) VALUES ('34046', 'Hand von Exi
 UPDATE `locales_item` SET `description_loc3`='Die Hand istd immer noch blutueberstroemt!' WHERE `entry`='34046';
 
 DELETE from `locales_item` where `entry` = 34047;
-INSERT INTO `wotlkworld`.`locales_item` (`entry`, `name_loc3`, `description_loc3`) VALUES ('34047', 'Beta Tester Belohnung', 'Hat die Betaphase der AvariusCore unterstuetzt!');
+INSERT INTO `locales_item` (`entry`, `name_loc3`, `description_loc3`) VALUES ('34047', 'Beta Tester Belohnung', 'Hat die Betaphase der AvariusCore unterstuetzt!');
