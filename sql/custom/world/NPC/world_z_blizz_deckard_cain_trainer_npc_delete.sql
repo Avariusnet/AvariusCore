bf501d42c60b6d571acbd1b4fc9f8aa6cc0ab5e3
Exitare <business.exitare@icloud.com>
2017-03-22 14:52:18 +0100

    NPC adjustments

---
+++ sql/custom/world/NPC/world_z_blizz_deckard_cain_trainer_npc_delete.sql
@@ -0,0 +1,8 @@
+/* delete Deckard Cain ID 90001 from blizz-realm*/
+
+DELETE FROM `creature` WHERE `guid`='90001';
+DELETE FROM `creature` WHERE `guid`='798011';
+DELETE FROM `creature` WHERE `guid`='798016';
+DELETE FROM `creature` WHERE `guid`='798019';
+DELETE FROM `creature` WHERE `guid`='798024';
+DELETE FROM `creature` WHERE `guid`='798030';
