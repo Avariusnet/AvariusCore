6fe856e87774d9c8469572209e5c872ea4a4877b
Exitare <business.exitare@icloud.com>
2017-03-28 13:11:48 +0200

    Fix a corrupted SQL

--- sql/custom/world/NPC/world_creature_loot_astral bones.sql
+++ sql/custom/world/NPC/world_creature_loot_astral bones.sql
@@ -1,75 +1,42 @@
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('29724', '33788', '0', '10', '1', '1', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('28407', '33788', '0', '2', '0', '1', '1', '1', '1');
-
+DELETE FROM `creature_loot_template` where `item` = 33788;
 UPDATE `creature_template` SET `lootid`='28407' WHERE `entry`='28407';
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('27254', '33788', '0', '9', '0', '1', '0', '1', '1');
 
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('20324', '33788', '0', '5', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('19402', '33788', '0', '5', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('19434', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('19261', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('19136', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('18978', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('3242', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('3243', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('3415', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('3248', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('3466', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('3236', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('3238', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('5827', '33788', '0', '5', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('830', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('126', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('456', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('171', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('513', '33788', '00', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('458', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('517', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('127', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('1216', '33788', '0', '2', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('391', '33788', '0', '5', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('6519', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('14460', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('6521', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('6520', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('9164', '33788', '0', '3', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('36620', '33788', '0', '10', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('36499', '33788', '0', '10', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('36564', '33788', '0', '10', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('36478', '33788', '0', '10', '0', '1', '0', '1', '1');
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('36666', '33788', '0', '15', '0', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`,`QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('29724', '33788', '0', '10','0', '1', '1', '1', '1'),
+('28407', '33788', '0', '2', '0', '1', '1', '1', '1'),
+('27254', '33788', '0', '9', '0', '1', '0', '1', '1'),
+('20324', '33788', '0', '5', '0', '1', '0', '1', '1'),
+('19434', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('19402', '33788', '0', '5', '0', '1', '0', '1', '1'),
+('19261', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('19136', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('18978', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('3242', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('3243', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('3415', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('3248', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('3466', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('3236', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('3238', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('5827', '33788', '0', '5', '0', '1', '0', '1', '1'),
+('830', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('126', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('456', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('171', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('513', '33788', '00', '2', '0', '1', '0', '1', '1'),
+('458', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('517', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('127', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('1216', '33788', '0', '2', '0', '1', '0', '1', '1'),
+('391', '33788', '0', '5', '0', '1', '0', '1', '1'),
+('6519', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('14460', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('6521', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('6520', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('9164', '33788', '0', '3', '0', '1', '0', '1', '1'),
+('36620', '33788', '0', '10', '0', '1', '0', '1', '1'),
+('36499', '33788', '0', '10', '0', '1', '0', '1', '1'),
+('36564', '33788', '0', '10', '0', '1', '0', '1', '1'),
+('36478', '33788', '0', '10', '0', '1', '0', '1', '1'),
+('36666', '33788', '0', '15', '0', '1', '0', '1', '1');
 
